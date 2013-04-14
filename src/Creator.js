b1b0f0e1810106348e016b28df443f827f413d29
Eli Mallon <ekmallon@gmail.com>
2013-04-14 02:12:05 -0700

    Creator uses the UI callback to register its callbacks.

--- src/Creator.js
+++ src/Creator.js
@@ -23,11 +23,12 @@ goog.provide("PAC.Creator");
             self.engine = new PAE.Game(game, data);
             $('canvas').bind('contextmenu', function(e){
                 return false;
-            }); 
-            self.initUI();
+            });
         })
         $(document).ready(function() {
-            self.ui = new PAC.UI("#GameAccordion");
+            self.ui = new PAC.UI("#GameAccordion", function() {
+                self.initUI();
+            });
         })
     };
     Creator.prototype.initUI = function() {
@@ -41,41 +42,6 @@ goog.provide("PAC.Creator");
             $("#AdminPanel").css('display', 'none');
             $("#ShowAdmin").css('display', 'block');
             $("#GameBorder").removeClass('right');
-        })
-        $('#showWalkable').on('click', function(e) {
-            var btn = $('#showWalkable');
-            var doThing = btn.attr("data-toggle");
-            if (doThing == 'true') {
-                PAE.curGame.curRoom.walkableDebug(true);
-                btn.attr('data-toggle', "false");
-                btn.html("Hide Walkable Path");
-            } else {
-                PAE.curGame.curRoom.walkableDebug(false);
-                btn.attr('data-toggle', "true");
-                btn.html("Show Walkable Path");
-            }
-        })
-        $('#showPathing').on('click', function(e) {
-            var btn = $('#showPathing');
-            var doThing = btn.attr("data-toggle");
-            if (doThing == 'true') {
-                PAE.curGame.curRoom.pathingDebug(true);
-                btn.attr('data-toggle', "false");
-                btn.html("Hide Pathfinding Data");
-            } else {
-                PAE.curGame.curRoom.pathingDebug(false);
-                btn.attr('data-toggle', "true");
-                btn.html("Show Pathfinding Data");
-            }
-        })
-        $('#rebuildPathing').on('click', function(e) {
-            PAE.curGame.curRoom.walkable.buildWalkGraph();
-        })
-        $('#SaveGame').on('click', function(e) {
-            var data = {data: self.engine.serialize()};
-            $.ajax({method: "POST", data: data}).done(function(game) {
-                
-            })
         })
     }
 })();
